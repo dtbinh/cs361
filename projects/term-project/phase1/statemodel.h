d7c7c4886546f3552e45b21ba877de9cbc63e0d8
lyonsje <lyonsje@dukes.jmu.edu>
2015-10-08 20:36:16 -0400

    Update statemodel.h

--- projects/term-project/phase1/statemodel.h
+++ projects/term-project/phase1/statemodel.h
@@ -1,21 +1,22 @@
 /*
  * statemodel.h
  *
- *  Created on: Sep 14, 2015
+ *  Created on: Oct 08, 2015
  *      Author: joshualyons
  */
 
 #ifndef statemodel_h
 #define statemodel_h
 #include <stdlib.h>
-#include "hardware.h"
+#include "actions.h"
 #include "state.h"
 
 extern state_t accepting;
 extern state_t processing;
 extern state_t manufacturing;
-extern state_t shippping;
+extern state_t shipping;
 
 void handle_event(event current_event);
 
 #endif
+
