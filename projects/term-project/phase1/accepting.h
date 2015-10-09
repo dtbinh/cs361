b7504cefcfc9d8f90aa63100830035d003ccc7ef
lyonsje <lyonsje@dukes.jmu.edu>
2015-10-08 20:24:43 -0400

    Update and rename closed.h to accepting.h

---
+++ projects/term-project/phase1/accepting.h
@@ -0,0 +1,16 @@
+/*
+ * accepting.h
+ *
+ *  Created on: Sep 14, 2015
+ *      Author: Joshua Lyons
+ */
+
+#ifndef ACCEPTING_H_
+#define ACCEPTING_H_
+
+#include "state.h"
+
+static state_t* order_recieved();
+
+#endif
+
