1b23ef6f2153d8a99848d1aee48c2faa8aa7bd14
Conner Turnbull <conner.turnbull@gmail.com>
2015-10-15 12:40:59 -0400

    hakljflskfjsalfd

---
+++ labs/lab04/client.c
@@ -0,0 +1,2 @@
+/*
+ * CS-450 Fall 2015 Demo of IPC using 
---
+++ labs/lab04/shmem-ex.h
@@ -0,0 +1,17 @@
+/*
+ * CS-450 Fall 2015
+ * Demo of IPC using Shared Memory
+ * Written By:
+ *  1- Dr. Mohamed Aboutabl
+ *  Submitted on:
+ */
+typedef struct {
+    int started;
+    int finished;
+    double d1;
+    double d2;
+} shared_data;
+
+#define SHMEM_KE 0x100
+#define SHMEM_SIZE sizeof(shared_data)
+
