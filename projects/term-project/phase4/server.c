3ef8949638928c01a302fdc43ec82bdcd4f7f10a
lyonsje <lyonsje@dukes.jmu.edu>
2015-12-07 00:23:50 -0500

    Update server.c

--- projects/term-project/phase4/server.c
+++ projects/term-project/phase4/server.c
@@ -1,72 +1,63 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <errno.h>
-#include <stdlib.h>
-
-#include "mySock.h"
-#define MAXBUFLEN   256
-
-/*------------------------------------------------------------------------
- * main - Iterative UDP server for DAYTIME service
- *------------------------------------------------------------------------
+/*
+ * server.c
+ *
+ * Modified on: Dec 6, 2015
+ *      Author: Joshua Lyons and Conner Turnbull (Group 1)
  */
 
-int main(int argc, char *argv[])
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "mySock.h"
+#include "actions.h"
+#include "message.h"
+
+int main(int argc, char** argv)
 {
-  struct sockaddr_in  fsin;   /* the from address of a client */
-  unsigned short port = 13 ;  /* service name or port number  */
-  char    buf[MAXBUFLEN];     /* "input" buffer; any size > 0 */
-  int	    sock;               /* server socket                */
-  time_t	now;                /* current time                 */
-  char    timeStr[MAXBUFLEN]; /* time string   */
-  unsigned int    alen;       /* from-address length          */
-	int ii, order_size, capacity, duration, lines_active;
-  
-  switch (argc) 
-    {
-    case 1:
-      break;
-    case 2:
-      port = atoi( argv[1] );
-      break;
-    default:
-      snprintf(buf, MAXBUFLEN , "usage: %s [port]\n" , argv[0] );
-      err_quit( buf ) ;
-    }
-  
-	srandom(time(NULL));
-  sock = serverUDPsock(port);
-  lines_active = 0;
-	order_size = (random() % 1001) + 1000;
-  
-  while (1) 
-    {
-      alen = sizeof(fsin);
+	int curLines = 5;
+	int iters[5], prod[5], cap[5], dur[5];
 
-      if ( recvfrom( sock, buf, MAXBUFLEN , 0, (SA *) &fsin, &alen ) < 0 )
-        err_sys( "recvfrom" );
+	msgBuf msg;
 
-      fprintf(stderr , "DAYTIME server received '%s'\n" , buf ) ;
-      if (strcmp(buf, "start") == 0)    /* Factory line is ready to begin */
-        {
-          lines_active++;
-          duration = (random() % 5) + 1;    //sets random duration between 1-5
-          capacity = (random() % 41) + 10;  //sets random capacity between 10-50
+	for (int ii = 0; ii < 5; ii++)
+	{
+		iters[ii] = 0;
+		prod[ii] = 0;
+		cap[ii] = 0;
+		dur[ii] = 0;
+	}
 
-          sendto( sock , (char *) &timeStr , strlen(timeStr) , 0 ,
-                 (SA *) &fsin, alen );
-          duration = -1;
-          capacity = -1;
-        }
-      
-      time( &now ); /* get the current system's time */
-      ctime_r( &now , timeStr );    /* WARNING! ctime() is NOT thread-safe */
-      fprintf(stderr , "DAYTIME server sending '%s'\n" , timeStr ) ;
-      sendto( sock , (char *) &timeStr , strlen(timeStr) , 0 , 
-             (SA *) &fsin, alen );
-    }
+	printf("Server is Waiting on Client...\n");
+
+	while(curLines > 0) {
+			if (msg.info.quit == 1)
+			{
+				iters[msg.info.id - 1]++;
+				cap[msg.info.id - 1] = msg.info.cap;
+				dur[msg.info.id - 1] = msg.info.dur;
+				prod[msg.info.id - 1] += msg.info.prod;
+			}
+			else if (msg.info.quit == 2)
+			{
+				curLines--;
+			}
+			else
+			{
+				printf("ERROR!\n");
+			}
+	}
+
+	for (int ii = 0; ii < 5; ii++)
+	{
+		printf("\nFactory Line: %d\n", ii+1);
+		printf("\tProduction: %d\n", prod[ii]);
+		printf("\tCapacity: %d\n", cap[ii]);
+		printf("\tIterations: %d\n", iters[ii]);
+		printf("\tDuration: %d\n", dur[ii]);
+	}
+
+	printf("\nSERVER GOING OFFLINE\n");
+	return 0;
 }
