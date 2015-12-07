493e96ed961df1835f7754180b11f593ca24c3df
lyonsje <lyonsje@dukes.jmu.edu>
2015-12-07 00:37:17 -0500

    Update mySock.h

--- projects/term-project/phase4/mySock.h
+++ projects/term-project/phase4/mySock.h
@@ -4,36 +4,40 @@
  * Modified on: Dec 6, 2015
  *      Author: Joshua Lyons and Conner Turnbull (Group 1)
  */
+#ifndef MESSAGE_H_
+#define MESSAGE_H_
 
 void err_quit(const char* x);
 void err_sys(const char* x);
 int clientUDPsock(const char *host, const char *service ) ;
 int serverUDPsock(const unsigned short port) ;
 
-typedef struct 
+typedef struct
 {
 	long mestype;
-	struct 
-	{
-		int id;
-		int cap; 
-		int items;
-		int dur;
-		int iters;
-	} info;
-} actionsMsg;
-
-typedef struct 
-{
-	long mestype;
-	struct 
+	struct
 	{
 		int id;
 		int cap;
 		int items;
 		int dur;
-	} info;
+		int iters;
+	} data;
+} actionsMsg;
+
+typedef struct
+{
+	long mestype;
+	struct
+	{
+		int id;
+		int cap;
+		int items;
+		int dur;
+	} data;
 } serverMsg;
 
 typedef struct sockaddr SA ;
 #define BASEPORT 50000
+
+#endif
