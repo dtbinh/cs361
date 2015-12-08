de053b374c8697270ba887ce7aae26e9c5e1a4dd
Conner Turnbull <conner.turnbull@gmail.com>
2015-12-08 16:48:08 -0500

    akdfj

--- projects/term-project/phase4/server.c
+++ projects/term-project/phase4/server.c
@@ -14,6 +14,11 @@
 int main(int argc, char *argv[])
 {
   struct sockaddr_in  fsin;   /* the from address of a client */
+  struct sockaddr_in  fsin1;   /* the from address of a client */
+  struct sockaddr_in  fsin2;   /* the from address of a client */
+  struct sockaddr_in  fsin3;   /* the from address of a client */
+  struct sockaddr_in  fsin4;   /* the from address of a client */
+  struct sockaddr_in  fsin5;   /* the from address of a client */
   unsigned short port = 13 ;  /* service name or port number  */
   char    buf[MAXBUFLEN];     /* "input" buffer; any size > 0 */
   msgBuf from_msg;            /* message received             */
@@ -49,6 +54,71 @@ int main(int argc, char *argv[])
 
   printf("Order size: %d\n", order_size);
   
+  /* NEW LINE */
+  if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
+    err_sys( "recvfrom" );
+  fsin1 = fsin;
+
+  lines_active++;
+  to_msg.info.factory_ID = lines_active;
+  to_msg.info.capacity = (random() % 41) + 10;
+  to_msg.info.duration = /*(random() % 5) + 1*/ 1;
+  to_msg.info.produce = to_msg.info.capacity;
+
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin1, alen );
+  
+  /* NEW LINE */
+  if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
+    err_sys( "recvfrom" );
+  fsin2 = fsin;
+
+  lines_active++;
+  to_msg.info.factory_ID = lines_active;
+  to_msg.info.capacity = (random() % 41) + 10;
+  to_msg.info.duration = /*(random() % 5) + 1*/ 1;
+  to_msg.info.produce = to_msg.info.capacity;
+
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin2, alen );
+  
+  /* NEW LINE */
+  if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
+    err_sys( "recvfrom" );
+  fsin3 = fsin;
+
+  lines_active++;
+  to_msg.info.factory_ID = lines_active;
+  to_msg.info.capacity = (random() % 41) + 10;
+  to_msg.info.duration = /*(random() % 5) + 1*/ 1;
+  to_msg.info.produce = to_msg.info.capacity;
+
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin3, alen );
+  
+  /* NEW LINE */
+  if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
+    err_sys( "recvfrom" );
+  fsin4 = fsin;
+
+  lines_active++;
+  to_msg.info.factory_ID = lines_active;
+  to_msg.info.capacity = (random() % 41) + 10;
+  to_msg.info.duration = /*(random() % 5) + 1*/ 1;
+  to_msg.info.produce = to_msg.info.capacity;
+
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin4, alen );
+  
+  /* NEW LINE */
+  if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
+    err_sys( "recvfrom" );
+  fsin5 = fsin;
+
+  lines_active++;
+  to_msg.info.factory_ID = lines_active;
+  to_msg.info.capacity = (random() % 41) + 10;
+  to_msg.info.duration = /*(random() % 5) + 1*/ 1;
+  to_msg.info.produce = to_msg.info.capacity;
+
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin5, alen );
+  
   while (!done) 
     {
       alen = sizeof(fsin);
@@ -56,66 +126,41 @@ int main(int argc, char *argv[])
       if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
         err_sys( "recvfrom" );
 
-      /* New factory line */
-      if (from_msg.info.factory_ID == 1234 && lines_active < 5)
+      to_msg.info.factory_ID = from_msg.info.factory_ID;
+      to_msg.info.capacity = from_msg.info.capacity;
+      to_msg.info.duration = from_msg.info.duration;
+      if (parts_remaining < 1) // if done
         {
-          lines_active++;
-          printf("LINES ACTIVE: %d\n", lines_active);
-          to_msg.info.factory_ID = lines_active;
-          to_msg.info.capacity = (random() % 41) + 10;
-          to_msg.info.duration = /*(random() % 5) + 1*/ 1;
-          to_msg.info.produce = to_msg.info.capacity;
+          done = 1;
+        }
+      else if ((parts_remaining - from_msg.info.capacity) >= 0) // if this isn't last order
+        {
+          to_msg.info.produce = from_msg.info.capacity;
+          parts_remaining -= from_msg.info.produce;
+          total_produced += from_msg.info.capacity;
           sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin, alen );
-
-          printf ("Line %d started: Capacity: %d Duration: %d Produce: %d\n", lines_active, to_msg.info.capacity, to_msg.info.duration, to_msg.info.produce);
         }
-      else
+      else //if this is last order
         {
-
-          to_msg.info.factory_ID = from_msg.info.factory_ID;
-          to_msg.info.capacity = from_msg.info.capacity;
-          to_msg.info.duration = from_msg.info.duration;
-          if (parts_remaining < 1) // if done
-            {
-              done = 1;
-            }
-          else if ((parts_remaining - from_msg.info.capacity) >= 0) // if this isn't last order
-            {
-              to_msg.info.produce = from_msg.info.capacity;
-              parts_remaining -= from_msg.info.produce;
-              total_produced += from_msg.info.capacity;
-              sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin, alen );
-            }
-          else //if this is last order
-            {
-              to_msg.info.produce = parts_remaining;
-              total_produced += parts_remaining;
-              parts_remaining = 0;
-              sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin, alen );
-            }
+          to_msg.info.produce = parts_remaining;
+          total_produced += parts_remaining;
+          parts_remaining = 0;
+          sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin, alen );
         }
-      
-      if (done)
-        {
-          while (lines_active > 0)
-            {
-              if ( recvfrom( sock, (void *)&from_msg, sizeof(from_msg), 0, (SA *) &fsin, &alen ) < 0 )
-                err_sys( "recvfrom" );
-
-              printf("LOOPS!\n");
-              to_msg.info.factory_ID = from_msg.info.factory_ID;
-              to_msg.info.capacity = from_msg.info.capacity;
-              to_msg.info.duration = from_msg.info.duration;
-              to_msg.info.produce = 0;
-
-              sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin, alen );
-              lines_active--;
-            }
-        }
-
-      printf ("Parts Remaining: %d Total Produced: %d\n", parts_remaining, total_produced);
     }
+      
+  to_msg.info.factory_ID = from_msg.info.factory_ID;
+  to_msg.info.capacity = from_msg.info.capacity;
+  to_msg.info.duration = from_msg.info.duration;
+  to_msg.info.produce = 0;
+      
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin1, alen );
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin2, alen );
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin3, alen );
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin4, alen );
+  sendto( sock , (void *) &to_msg, sizeof(to_msg), 0, (SA *) &fsin5, alen );
 
+  printf ("Parts Remaining: %d Total Produced: %d\n", parts_remaining, total_produced);
   printf ("Total items produced: %d\n", total_produced);
   while (1);
 }
